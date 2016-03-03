a5fc85fcd71eae9c975045728e1e46abbbd65d1d
0x4D61696B <365945ba@gmail.com>
2016-03-03 07:06:31 +0100

    Initial commit

---
+++ bGroupCount.lua
@@ -0,0 +1,89 @@
+-- =============================================================================
+--  bGroupCount
+--    by: BurstBiscuit
+-- =============================================================================
+
+require "math"
+require "table"
+require "unicode"
+require "lib/lib_Callback2"
+require "lib/lib_ChatLib"
+require "lib/lib_Debug"
+require "lib/lib_HudManager"
+require "lib/lib_InterfaceOptions"
+require "lib/lib_Slash"
+
+Debug.EnableLogging(false)
+
+
+-- =============================================================================
+--  Variables
+-- =============================================================================
+
+local FRAME = Component.GetFrame("Main")
+local COUNT = Component.GetWidget("Count")
+
+
+-- =============================================================================
+--  Functions
+-- =============================================================================
+
+function Notification(message)
+    ChatLib.Notification({text = "[bGroupCount] " .. tostring(message)})
+end
+
+function UpdateGroupCount()
+    if (Platoon.IsInPlatoon()) then
+        COUNT:SetText("Platoon (" .. tostring(#Platoon.GetRoster().members) .. "/" .. tostring(Platoon.GetMaxPlatoonSize()) .. ")")
+        FRAME:Show(true)
+    elseif (Squad.IsInSquad()) then
+        COUNT:SetText("Squad (" .. tostring(#Squad.GetRoster().members) .. "/" .. tostring(Squad.GetMaxSquadSize()) .. ")")
+        FRAME:Show(true)
+    else
+        FRAME:Show(false)
+    end
+end
+
+function OnHudShow(show, dur)
+    FRAME:ParamTo("alpha", tonumber(show), dur)
+end
+
+function OnSlashCommand(args)
+    Notification("Showing test values for the text frame")
+
+    Callback2.FireAndForget(function()
+        COUNT:SetText("Test (" .. math.random(1, tonumber(Platoon.GetMaxPlatoonSize())) .. "/" .. tostring(Platoon.GetMaxPlatoonSize()) .. ")")
+        FRAME:Show(true)
+    end, nil, 1)
+
+    Callback2.FireAndForget(function()
+        UpdateGroupCount()
+    end, nil, 11)
+end
+
+
+-- =============================================================================
+--  Events
+-- =============================================================================
+
+function OnComponentLoad()
+    InterfaceOptions.SetCallbackFunc(nil)
+    InterfaceOptions.AddMovableFrame({
+        frame = FRAME,
+        label = "bGroupCount",
+        scalable = true,
+    })
+
+    HudManager.BindOnShow(OnHudShow)
+
+    LIB_SLASH.BindCallback({
+        slash_list = "bgroupcount",
+        description = "bGroupCount",
+        func = OnSlashCommand
+    })
+end
+
+function OnSquadRosterUpdate(args)
+    Debug.Table("OnSquadRosterUpdate()", args)
+    UpdateGroupCount()
+end
